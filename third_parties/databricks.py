a2cb4adf216c95cdf0ce4be21001c75dd56475f3
msingla403 <msingla.developer@gmail.com>
2023-08-04 13:18:50 +0200

    token limit

--- third_parties/databricks.py
+++ third_parties/databricks.py
@@ -7,3 +7,13 @@ def get_documentation(name: str):
     for doc in docs.load():
         content = doc.page_content
     return content
+
+
+def get_full_documentation(name: str):
+    docs = TextLoader(
+        "/Users/mohit/Documents/sarvam/langChain/docs/full_documentation.txt"
+    )
+    content = []
+    for doc in docs.load():
+        content = doc.page_content
+    return content
